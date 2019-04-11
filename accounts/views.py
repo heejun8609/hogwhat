ed5a39a1d66bbbd4380c91e530941756c0556593
morris <morris@aidkr.com>
2019-04-11 16:20:50 +0900

    nginx_update

--- accounts/views.py
+++ accounts/views.py
@@ -16,10 +16,10 @@ class UserInfoView(APIView):
         사용자 정보를 받는다.
 
         <p><b> user_key [STRING]: </b>사용자 key</p>
-        <p><b> animal_cateogry [STRING]: </b>축종</p>
+        <p><b> species [STRING]: </b>축종</p>
         <p><b> area [STRING]: </b>지역</p>
-        <p><b> animal_count [STRING]: </b>사육두수</p>
-        <p><b> phone_number [STRING]: </b>핸드폰 번호</p>
+        <p><b> scale [STRING]: </b>사육두수</p>
+        <p><b> phone [STRING]: </b>핸드폰 번호</p>
         """
         data = request.data
 
@@ -28,16 +28,18 @@ class UserInfoView(APIView):
         else:
             user = request.user
         ip = request.META['REMOTE_ADDR']
-        animal_cateogry = data.get('animal_category')
+        species = data.get('species')
         area = data.get('area')
-        animal_count = data.get('animal_count')
-        if data.get('phone_number'):
-            phone_number = str(data.get('phone_number'))
-            upload_user_info(user, ip, animal_cateogry, area, animal_count, phone_number=phone_number)
+        scale = data.get('scale')
+        if data.get('phone'):
+            phone = str(data.get('phone'))
+            upload_user_info(user, ip, species, area, scale, phone=phone)
         else:
-            upload_user_info(user, ip, animal_cateogry, area, animal_count)
+            upload_user_info(user, ip, species, area, scale)
         logger.debug('User Info Upload')
         return Response(status=status.HTTP_200_OK)
+
+        
 # @login_required
 def profile(request):
     return render(request, 'login.html')
